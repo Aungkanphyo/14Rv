0c882a44f16c6be72adfff9361b2e683add90896
aungkanphyoo <aungkanphyo1@gmail.com>
2024-04-22 16:55:32 +0630

    add Person.prototype.getName

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ class Person {
   }
 }
 
+Person.prototype.getName = function () {
+  console.log(`My real name is ${this.name}.`);
+};
+
 const person1 = new Person("Mg Mg", 21, "mgmg@gmail.com");
 person1.getAge();
+person1.getName();
 
 class VipPerson extends Person {
   constructor(name, age, email, vipTicket) {
@@ -25,3 +30,6 @@ class VipPerson extends Person {
 const vip1 = new VipPerson("Aung Aung", 21, "aung@gmail.com", "ejwfoenwpeif");
 vip1.getAge();
 vip1.getRoomFree();
+vip1.getName();
+// vip1 ကို output ထုတ်ကြည့်လိုက်ရင် console မှာ prototype ဆိုတဲ့ကောင်လေးပါတယ်။အ့တာကိုသိချင်လို့ output ထုတ်ထားတာဖြစ်တယ်။
+console.log(vip1);
